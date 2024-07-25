1fe24cbc072b59cb3ee2dff753ea8f097b45cd2d
ElVit <54866762+ElVit@users.noreply.github.com>
2024-07-25 12:16:09 +0200

    Update status.php

--- src/tasmotapm/usr/local/emhttp/plugins/tasmotapm/status.php
+++ src/tasmotapm/usr/local/emhttp/plugins/tasmotapm/status.php
@@ -1,28 +1,28 @@
 <?php
-$tasmotapm_cfg = parse_ini_file( "/boot/config/plugins/tasmotapm/tasmotapm.cfg" );
-$tasmotapm_device_ip	= isset($tasmotapm_cfg['DEVICE_IP']) ? $tasmotapm_cfg['DEVICE_IP'] : "";
-$tasmotapm_use_pass	= isset($tasmotapm_cfg['DEVICE_USE_PASS']) ? $tasmotapm_cfg['DEVICE_USE_PASS'] : "false";
-$tasmotapm_device_user	= isset($tasmotapm_cfg['DEVICE_USER']) ? $tasmotapm_cfg['DEVICE_USER'] : "";
-$tasmotapm_device_pass	= isset($tasmotapm_cfg['DEVICE_PASS']) ? $tasmotapm_cfg['DEVICE_PASS'] : "";
-$tasmotapm_costs_price	= isset($tasmotapm_cfg['COSTS_PRICE']) ? $tasmotapm_cfg['COSTS_PRICE'] : "0.0";
-$tasmotapm_costs_unit	= isset($tasmotapm_cfg['COSTS_UNIT']) ? $tasmotapm_cfg['COSTS_UNIT'] : "USD";
+$openbekenpm_cfg 		= parse_ini_file( "/boot/config/plugins/openbekenpm/openbekenpm.cfg" );
+$openbekenpm_device_ip		= isset($openbekenpm_cfg['DEVICE_IP']) ? $openbekenpm_cfg['DEVICE_IP'] : "";
+$openbekenpm_use_pass		= isset($openbekenpm_cfg['DEVICE_USE_PASS']) ? $openbekenpm_cfg['DEVICE_USE_PASS'] : "false";
+$openbekenpm_device_user	= isset($openbekenpm_cfg['DEVICE_USER']) ? $openbekenpm_cfg['DEVICE_USER'] : "";
+$openbekenpm_device_pass	= isset($openbekenpm_cfg['DEVICE_PASS']) ? $openbekenpm_cfg['DEVICE_PASS'] : "";
+$openbekenpm_costs_price	= isset($openbekenpm_cfg['COSTS_PRICE']) ? $openbekenpm_cfg['COSTS_PRICE'] : "0.0";
+$openbekenpm_costs_unit		= isset($openbekenpm_cfg['COSTS_UNIT']) ? $openbekenpm_cfg['COSTS_UNIT'] : "USD";
 
 
-if ($tasmotapm_device_ip == "") {
-	die("Tasmota Device IP missing!");
+if ($openbekenpm_device_ip == "") {
+	die("OpenBeken Device IP missing!");
 }
 
-$Url = "http://" . $tasmotapm_device_ip . "/cm?";
+$Url = "http://" . $openbekenpm_device_ip . "/cm?";
 
-if ($tasmotapm_use_pass == 1) {
-	if ($tasmotapm_device_user == "") {
-		die("Tasmota username missing!");
+if ($openbekenpm_use_pass == 1) {
+	if ($openbekenpm_device_user == "") {
+		die("OpenBeken username missing!");
 	}
-	if ($tasmotapm_device_pass == "") {
-		die("Tasmota password missing!");
+	if ($openbekenpm_device_pass == "") {
+		die("OpenBeken password missing!");
 	}
 
-	$Url = $Url . "user=" . $tasmotapm_device_user . "&password=" . $tasmotapm_device_pass . "&";
+	$Url = $Url . "user=" . $openbekenpm_device_user . "&password=" . $openbekenpm_device_pass . "&";
 }
 
 $Url = $Url . "cmnd=Status%208";
@@ -40,8 +40,8 @@ $json = array(
 		'ReactivePower' => $data['StatusSNS']['ENERGY']['ReactivePower'],
 		'Factor' => $data['StatusSNS']['ENERGY']['Factor'],
 		'Power' => $data['StatusSNS']['ENERGY']['Power'],
-		'Costs_Price' => $tasmotapm_costs_price,
-		'Costs_Unit' => $tasmotapm_costs_unit
+		'Costs_Price' => $openbekenpm_costs_price,
+		'Costs_Unit' => $openbekenpm_costs_unit
 	);
 
 header('Content-Type: application/json');
